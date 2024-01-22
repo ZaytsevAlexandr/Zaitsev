b562f119d74f99337d38a09eb0dc4e78dd0ff2ab
Zaytsev Aleksandr <145544514+ZaytsevAlexandr@users.noreply.github.com>
2024-01-22 21:48:59 +0300

    Update string.cpp
    changed

--- c++ github/string.cpp
+++ c++ github/string.cpp
@@ -1,108 +1,81 @@
+// Дана строка, содержащая от 1 до 30 слов, в каждом из которых от 1 до 10 латинских букв и/или цифр;
+// между соседними словами - не менее одного пробела, за последним словом - точка.
+// Напечатать все слова, отличные от последнего слова, предварительно преобразовав
+// каждое по правилу: оставить в слове только первое вхождение каждой буквы.
+
+
     #include <iostream>
     #include <cstring>
 
     using namespace std;
 
-    struct Vector
-    {
-        char* data;
-        int _size; // current size
-        int capacity; // maximum number of available elements
-
-        Vector(int start_size = 100){ // Create a vector (construct)
-            data = new char[start_size];
-            capacity = start_size;
-            _size = 0;
-            // cout << "New vector has been created!" << '\n'; // test
-        }
-
-        void push(char element){ // Add an element to the end
-            if (_size == capacity){
-                capacity = (capacity ? capacity * 2 : 1); // if (capacity != 0) then? (capacity * 2) else: 1
-                char* new_data = new char[capacity];
-                for (int i = 0; i < _size; i++)
-                    new_data[i] = data[i];
-                delete[] data;
-                data = new_data;
-            }
-            data[_size++] = element;
-        }
-
-        void throw_vector(){ // Print out an array
-            for (int i = 0; i < _size; i++)
-                cout << data[i] << ' ';
-            cout << '\n';
-        }
-
-        char last_word(){
-            char LastWord[10];
-            int CurrentNumberOfLetters = 0;
-
-            for (int j = _size - 1;;j--){
-                int i = 0;
-                if (int(data[j]) != 32){
-                    CurrentNumberOfLetters++;
-                    LastWord[10 - i++] = data[j];
-                }
-                else break;
-            }
-
-            if (CurrentNumberOfLetters != 10){
-                for (int i = 0; i < 10; i++){
-                    int j = 0;
-
-                    LastWord[j] = LastWord[10 - CurrentNumberOfLetters + j];
-                    j++;
-                }
-
-                for (int i = CurrentNumberOfLetters; i < 10; i++)
-                    LastWord[i] = '\0';
-            }
-
-            return *LastWord;
-        }
-
-        void task(){
-            char words[30][10];
-            int CurrentSymbol = 0;
-
-            int NumberOfWords = 0;
-            for (int Word = 0; Word < 30; Word++){
-                if (data[CurrentSymbol] != 32){
-                    for (int Letter = 0;; Letter++){
-                        words[Word][Letter] = data[CurrentSymbol];
-                        if (int(data[++CurrentSymbol]) == 32)
-                            break;
-                    }
-
-                    NumberOfWords++;
-                }
-
-                else{
-                    CurrentSymbol++;
-                    continue;
+    string string_push(){
+        cout << "You must use only uppercase and lowercase Latin letters and numbers, and you must put the dot in the end of your string!" << endl;
+        string start_str;
+        while (true) {
+            getline(cin, start_str);
+            bool valid = true;
+            for (char c : start_str) {
+                if (!isalnum(c) && c != ' ' && c != '.') {
+                    valid = false;
+                    break;
                 }
             }
-            for (int i = 0; i < NumberOfWords; i++){
-                if (strcmp(words[i], last_word())){
-
-                }
+            if (valid && start_str.back() == '.') {
+                break;
+            } else {
+                cout << "Please, try again" << endl;
             }
         }
-    };
+        return start_str;
+    }
 
+    void string_split(string str){
+        string words[30];
+        int index_LastWord = 0;
 
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~Разделение слов~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
+        char* char_str = new char[str.length() + 1];
+        strcpy(char_str, str.c_str());
+
+        char* pch = strtok (char_str, " ."); // во втором параметре указаны разделители (пробел, точка)
+        for (int i = 0; pch != nullptr; i++){
+            words[i] = pch; // сохраняем слово
+            index_LastWord = i;
+
+            pch = strtok (nullptr, " .");
+        }
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+        delete [] char_str;
+
+        for (int i = 0; i < index_LastWord; i++){ // перебор слов
+            if (words[i] == words[index_LastWord]){ // проверка на совпадение со последним словом
+                continue;
+            }
+
+            string new_word = ""; // новое слово
+            bool already_used_small[26] = {false}; // массив для отслеживания уже использованных маленьких букв
+
+            for (int j = 0; j < words[i].length(); j++){ // перебор букв в слове
+                if (!already_used_small[tolower(words[i][j]) - 'a']) { // проверка на первое вхождение буквы,
+                    new_word += words[i][j]; // добавляем символ в новое слово
+                    already_used_small[tolower(words[i][j]) - 'a'] = true; // отмечаем использование буквы
+                }
+            }
+            cout << new_word << endl;
+        }
+    }
 
     int main(){
-        Vector str;
+        string my_string;
+        my_string = string_push();
 
-        for (;;){ // enter the string
-            char symbol;
-            cin >> symbol;
-            str.push(symbol);
+        cout << "Your string isn't split:" << endl;
+        cout << my_string << endl;
 
-            if (symbol == '.')
-                break;
-        }
+        cout << "Your split string is written below:" << endl;
+        string_split(my_string);
     }
